4f8346f8fdfecd8ef7edc2fdf2b8a5b5e4351145
Namratha Alapati <namrathaalapati1@gmail.com>
2020-11-06 13:19:25 +0100

    added carsouel

--- src/Bride.js
+++ src/Bride.js
@@ -6,24 +6,23 @@ import Banner from "./Banner";
 import Nav from "./Nav";
 
 function Bride() {
-	return (
-		<div className="app">
-
-			<Nav />
-			<Banner />
-			<Row
-				title="NETFLIX ORIGINALS"
-				fetchUrl={request.fetchNetflixOriginals}
-				isLargeRow
-			/>
-			<Row title="Trending Now" fetchUrl={request.fetchTrending} />
-			<Row title="Top Rated" fetchUrl={request.fetchTopRated} />
-			<Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
-			<Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
-			<Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
-			<Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
-		</div >
-	);
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default Bride;
--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "./axios";
 import "./Row.css";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row(props) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+  const ref = useRef(null);
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(props.fetchUrl);
@@ -17,6 +23,10 @@ function Row(props) {
     }
     fetchData();
   }, [props.fetchUrl]);
+
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
   const opts = {
     height: "390",
     width: "100%",
@@ -26,7 +36,6 @@ function Row(props) {
     },
   };
   const handleClick = (movie) => {
-    debugger;
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
@@ -39,20 +48,36 @@ function Row(props) {
     }
   };
   return (
-    <div className="row">
+    <div className="posterrow">
       <h2>{props.title}</h2>
-      <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            className={`row__poster ${props.isLargeRow && "row__posterLarge"}`}
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            src={`${base_url}${
-              props.isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+      <div className="wrapper">
+        <div className="row__posters" ref={ref}>
+          <div
+            className={`angleLeft ${props.isLargeRow && "angleLeftLarge"}`}
+            onClick={() => scroll(-400)}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </div>
+          <div
+            className={`angleRight ${props.isLargeRow && "angleRightLarge"}`}
+            onClick={() => scroll(400)}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </div>
+          {movies.map((movie) => (
+            <img
+              className={`row__poster ${
+                props.isLargeRow && "row__posterLarge"
+              }`}
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              src={`${base_url}${
+                props.isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
       </div>
       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
