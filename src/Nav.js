95b40d784f4e5d5824afaa1778ff3df9184f7576
Padarthi <spadarth@rcinnet.rockwellcollins.com>
2020-11-10 20:44:53 +0530

    fixed prod issue

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,7 @@ import groom from "./Images/groom.png";
 
 function Nav() {
   const [show, handleShow] = useState(false);
-  const [showAvatar, handleShowAvatar] = useState(false);
+  // const [showAvatar, handleShowAvatar] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
@@ -20,21 +20,22 @@ function Nav() {
   }, []);
 
   const location = useLocation();
-  useEffect(() => {
-    if ((location.pathname !== "/browse") || (location.pathname !== "/invitation")) {
-      handleShowAvatar(false)
-    } else {
-      handleShowAvatar(true)
-    }
-  }, [location.pathname]);
+  // useEffect(() => {
+  //   if ((location.pathname !== "/browse") || (location.pathname !== "/invitation")) {
+  //     handleShowAvatar(false)
+  //   } else {
+  //     console.log("inside else");
+  //     handleShowAvatar(true)
+  //   }
+  // }, [location.pathname]);
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <Link to="/browse" >
         <img className="nav__logo" src={logo} alt="Netflix Logo" />
       </Link>
-      {showAvatar &&
-        <img className="nav__avatar" src={location.pathname === "/bride" ? bride : groom} alt="Netflix Logo" />
-      }
+      {/* { ((location.pathname !== "/browse") || (location.pathname !== "/invitation")) && */}
+      <img className="nav__avatar" src={location.pathname === "/bride" ? bride : location.pathname === "/groom" ? groom : null} alt="" />
+      {/* } */}
     </div>
   );
 }
