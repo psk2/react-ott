54ae3d78c685cf34ab626f195d5a9653b7d188db
Padarthi <spadarth@rcinnet.rockwellcollins.com>
2020-11-10 20:46:38 +0530

    removed chunk

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,8 @@ import groom from "./Images/groom.png";
 
 function Nav() {
   const [show, handleShow] = useState(false);
-  // const [showAvatar, handleShowAvatar] = useState(false);
+  const location = useLocation();
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
@@ -19,23 +20,12 @@ function Nav() {
     };
   }, []);
 
-  const location = useLocation();
-  // useEffect(() => {
-  //   if ((location.pathname !== "/browse") || (location.pathname !== "/invitation")) {
-  //     handleShowAvatar(false)
-  //   } else {
-  //     console.log("inside else");
-  //     handleShowAvatar(true)
-  //   }
-  // }, [location.pathname]);
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <Link to="/browse" >
         <img className="nav__logo" src={logo} alt="Netflix Logo" />
       </Link>
-      {/* { ((location.pathname !== "/browse") || (location.pathname !== "/invitation")) && */}
       <img className="nav__avatar" src={location.pathname === "/bride" ? bride : location.pathname === "/groom" ? groom : null} alt="" />
-      {/* } */}
     </div>
   );
 }
