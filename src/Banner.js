bea33bdcca21bb1544a803d874596f7cbdbb4cb4
Namratha Alapati <namrathaalapati1@gmail.com>
2020-11-11 16:06:00 +0100

    Adjusted the css to the banner and invitaion

--- src/Banner.js
+++ src/Banner.js
@@ -11,36 +11,40 @@ function Banner() {
       autoplay: 0,
     },
   };
-  const banner_descripion = `We may not have it all together, but together we have it all.`
+  const banner_descripion = `We may not have it all together, but together we have it all.`;
   const [showTitles, setShowTitles] = useState(false);
   const playTitles = () => {
-    setShowTitles(true)
+    setShowTitles(true);
   };
   return (
     <div>
-      <header
-        className="banner"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage: `url(${!showTitles && invitation2})`,
-          backgroundPosition: "center center",
-        }}
-      >
-        {!showTitles && <div className="banner__contents">
-          <div className="banner__contents">
-            <h1 className="banner__title">
-              SAHARI WEDDING
-             </h1>
-            <h1 className="banner__description">{banner_descripion}</h1>
-            <div className="banner__buttons">
-              <button className="banner__button banner_play_button" onClick={() => playTitles()}>Play</button>
-              <button className="banner__button">My List</button>
+      <header>
+        <div className="banner">
+          <img
+            className="banner__image"
+            src={`${!showTitles && invitation2}`}
+            alt="Invitation"
+          />
+
+          {!showTitles && (
+            <div className="banner__contents">
+              <h1 className="banner__title">SAHARI WEDDING</h1>
+              <h1 className="banner__description">{banner_descripion}</h1>
+              <div className="banner__buttons">
+                <button
+                  className="banner__button banner_play_button"
+                  onClick={() => playTitles()}
+                >
+                  Play
+                </button>
+                <button className="banner__button">My List</button>
+              </div>
             </div>
-          </div>
-        </div>}
-        {showTitles && <YouTube videoId="imY6QMDs4qU" opts={opts} />}
+          )}
+          {showTitles && <YouTube videoId="imY6QMDs4qU" opts={opts} />}
+        </div>
+        <div className="banner__fadeBottom"></div>
       </header>
-      <div className="banner__fadeBottom"></div>
     </div>
   );
 }
