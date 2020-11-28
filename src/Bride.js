368c78b0457db69bb223b9528827e99157f39d02
Padarthi <spadarth@rcinnet.rockwellcollins.com>
2020-11-28 10:13:09 +0530

    fixed anchor tag issue

--- src/Bride.js
+++ src/Bride.js
@@ -8,29 +8,31 @@ import Nav from "./Nav";
 import { useLocation } from "react-router-dom";
 
 function Bride() {
-  const location = useLocation();
-  let images = [];
-  if (location.pathname === "/bride") {
-    images = bride_images.brideImages
-  } else if (location.pathname === "/groom") {
-    images = groom_images.groomImages
-  }
-  return (
-    <div className="app">
-      <Nav />
-      <Banner />
-      <Row
-        title="Trending Now"
-        data={images}
-        isLargeRow
-      />
-      <div className="footer">
-        <footer className="location-text">
-          <a href="https://goo.gl/maps/dj7Zi3YTyNrwscJe9" target="_blank">You Can Find Venue Directions Here!</a>
-        </footer>
-      </div>
-    </div>
-  );
+	const location = useLocation();
+	let images = [];
+	if (location.pathname === "/bride") {
+		images = bride_images.brideImages
+	} else if (location.pathname === "/groom") {
+		images = groom_images.groomImages
+	}
+	return (
+		<div className="app">
+			<Nav />
+			<Banner />
+			<Row
+				title="Trending Now"
+				data={images}
+				isLargeRow
+			/>
+			<div className="footer">
+				<footer className="location-text">
+					<a href="https://goo.gl/maps/dj7Zi3YTyNrwscJe9" target="_blank" rel="noopener noreferrer">
+						You Can Find Venue Directions Here!
+					</a>
+				</footer>
+			</div>
+		</div>
+	);
 }
 
 export default Bride;
