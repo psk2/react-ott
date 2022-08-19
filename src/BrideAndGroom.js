736b0865c73c503ad06654849cc80d3ee1a0eaa2
Saikrishna PSK <psk.11594@gmail.com>
2022-08-19 10:55:03 +0530

    updated invitation

--- src/BrideAndGroom.js
+++ src/BrideAndGroom.js
@@ -8,35 +8,48 @@ import Nav from "./Nav";
 import { useLocation } from "react-router-dom";
 
 function BrideAndGroom() {
-	const location = useLocation();
-	let images = [];
-	if (location.pathname === "/bride") {
-		images = bride_images.brideImages
-	} else if (location.pathname === "/groom") {
-		images = groom_images.groomImages
-	}
-	return (
-		<div className="app">
-			<Nav />
-			<Banner />
-			<Row
-				title="Trending Now"
-				data={images}
-				isLargeRow
-			/>
-			<div className="footer">
-				<footer className="location-text">
-					<a href="https://goo.gl/maps/mNuXKj19VgmZCQuh6" target="_blank" rel="noopener noreferrer">
-						Click Here for <b><i>Wedding</i></b> Venue Directions <u>(Kasturi Devi Gardens)</u>
-					</a>
-					<br></br>
-					<a href="https://goo.gl/maps/s1PU6ZnmYCP7QHbj8" target="_blank" rel="noopener noreferrer">
-						Click Here for <b><i>Reception</i></b> Venue Directions <u>(Krishnam Rajeswaramma Town Hall)</u>
-					</a>
-				</footer>
-			</div>
-		</div>
-	);
+  const location = useLocation();
+  let images = [];
+  if (location.pathname === "/bride") {
+    images = bride_images.brideImages;
+  } else if (location.pathname === "/groom") {
+    images = groom_images.groomImages;
+  }
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="Trending Now" data={images} isLargeRow />
+      <div className="footer">
+        <footer className="location-text">
+          <a
+            href="https://goo.gl/maps/mNuXKj19VgmZCQuh6"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click Here for{" "}
+            <b>
+              <i>Wedding</i>
+            </b>{" "}
+            Venue Directions <u>(Kasturi Devi Gardens)</u>
+          </a>
+          <br></br>
+          <a
+            href="https://goo.gl/maps/s1PU6ZnmYCP7QHbj8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click Here for{" "}
+            <b>
+              <i>Reception</i>
+            </b>{" "}
+            Venue Directions <u>(Krishnam Rajeswaramma Town Hall)</u>
+          </a>
+          <div className="author">Made by Sai Krishna Padarthi</div>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default BrideAndGroom;
--- src/Invitation.js
+++ src/Invitation.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./invitation.css";
 import invitation1 from "./Images/invitations/invitation1.jpeg";
 import invitation2 from "./Images/invitations/invitation2.jpeg";
-// import invitation3 from "./Images/invitations/invitation3.jpeg";
+import invitation3 from "./Images/invitations/invitation3.jpeg";
 // import invitation4 from "./Images/invitations/invitation4.jpeg";
 import Nav from "./Nav";
 
@@ -19,12 +19,12 @@ function Invitation() {
           <img className="gallery__image" src={invitation2} alt="Invitation" />
           <div className="desc">Sreeja Weds Subramanyam</div>
         </div>
-        {/* commenting for future */}
-        {/* <div className="gallery">
+        <div className="gallery">
           <img className="gallery__image" src={invitation3} alt="Invitation" />
           <div className="desc">Subramanyam weds Sreeja</div>
         </div>
-        <div className="gallery">
+        {/* commenting for future */}
+        {/* <div className="gallery">
           <img className="gallery__image" src={invitation4} alt="Invitation" />
           <div className="desc">Subramanyam weds Sreeja</div>
         </div> */}
